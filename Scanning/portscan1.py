4a76eb8f3618f51c43b200c686c48fc553b30730
Logan Wheeler <35555839+BraysonWheeler@users.noreply.github.com>
2021-02-17 23:58:52 -0600

    Create portscan1.py

---
+++ Scanning/portscan1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# nmap allows scanning target for open ports running vulnerbal software allowing us to exploit them
+# this will scan for open ports and later see if software running is vulnerable
+# ex of nmap -> nmap [target ip]
+
+import socket #library we want to import
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET is for ipv4 and pv6 Sock Stream for tcp packets going for 3 way handshake
+
+host = "192.168.1.52" #target pc
+port = 443 #can be any port nnmap -sV [ip] for open ports
+
+#takes in argument port
+def portscanner(port):
+	if sock.connect((host,port)):
+		print "Port %d closed" % (port) #if error
+	else:
+		print "Port %d is open" % (port) #if success
+portscanner(port) #calling the function
